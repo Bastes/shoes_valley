c71be91047ed928878a371a4a9dca257f5aba193
Michel Belleville <michelbelleville@imac-de-michel-belleville.local>
2009-05-13 19:24:36 +0200

    Added partial documentation.

--- shoes_valley.rb
+++ shoes_valley.rb
@@ -1,11 +1,16 @@
+# gives boardable pieces the basics
 module Piece
-  attr_reader :x, :y, :board
+  attr_reader :x, :y, :board # has a board and coordinates
 
+	# board:: Board instance, required
+	# x, y::  coordinates, only if the piece is not out
   def initialize board, x = nil, y = nil
     @board = board
     self.put x, y
   end
 
+	# put the piece from wherever to a free position on the board
+	# x, y:: new position
   def put x, y
     unless @board.free? x, y
       raise ArgumentError.new("Cell #{x}, #{y} occupied.")
@@ -14,15 +19,18 @@ module Piece
     self
   end
 
+	# takes the piece out of the board as a prisonner
   def kill
     @x = @y = nil
     self
   end
 
+	# is the piece out of the board ?
   def dead?
     @x.nil?
   end
 
+	# can be compared to any of its own class, its own class or its symbol
   def == other
     if other == self.class || other.to_sym == :dwarf
       return true
@@ -31,6 +39,32 @@ module Piece
   end
 end
 
+=begin
+The Dwarf piece (D)
+
+Moves in any straight row, column or diagonal line on free cells to any
+free cell
+\  |  /
+ \ | /
+  \|/
+---D---
+  /|\
+ / | \
+/  |  \
+
+Can be hurled to land on and capture a troll as long as it's got as many
+supporting dwarves behind as cells between it and the troll.
+The cells between him and the troll should be free, in a straight row,
+column or diagonal line, and the support dwarves must be on the same line
+just behind it.
+DDD   T
+DD.-->D
+
+The board starts with dwarves all around the outmost cells of the board,
+except for the middle cells of each side.
+
+A captured dwarf awards 1 point to the Troll player.
+=end
 class Dwarf
   include Piece
 
@@ -49,6 +83,20 @@ class Dwarf
 	end
 end
 
+=begin
+The Troll piece (T)
+
+The troll piece move to any free adjacent cell.
+\|/
+-T-
+/|\
+
+A troll can be shoved to any free cell adjacent to at least one dwarf, with
+as much supporting trolls as cells between it and its destination cell. The
+cells crossed must be free, in any straight row, column or diagonal ligne,
+and the supporting trolls must be behind the moving troll.
+
+=end
 class Troll
   include Piece
 
