8a3dc02af6a25c6671a0e3fc3a541378a4b95b47
Michel Belleville <michel.belleville@gmail.com>
2009-05-14 22:22:36 +0200

    Almost equifunctional.

--- shoes_valley.rb
+++ shoes_valley.rb
@@ -20,35 +20,66 @@ module BoardView
     @height = value
   end
 
-  def redraw
-    self.image "./board.png", :top => 0, :left => 0,
-               :width => board_size, :height => board_size
+  def redraw all = false
+    if all or @pieces.nil?
+      self.clear do
+        self.image "./board.png", :top => 0, :left => 0,
+                   :width => board_size, :height => board_size
 
-    fill rgb(0, 0, 0, 0)
-    15.times do |y|
-      15.times do |x|
-        if @board.cell? x, y
-          zone = rect((cell_gap * x).to_i, (cell_gap * y).to_i,
-                      cell_size, cell_size)
-          # FIXME : controller method
+        fill rgb(0, 0, 0, 0)
+        15.times do |y|
+          15.times do |x|
+            if @board.cell? x, y
+              zone = rect((cell_gap * x).to_i, (cell_gap * y).to_i,
+                          cell_size, cell_size)
+              zone.click do
+                self.clicked x, y
+              end
+            end
+          end
         end
       end
+
+      @pieces = []
+    else
+      @pieces.each { |piece| piece.remove }
+      @pieces.empty
     end
 
     @board.each do |piece|
-      unless piece.dead?
-        piece_image piece
-      end
+      @pieces << piece_image(piece) unless piece.dead?
+    end
+  end
+
+  def clicked x, y
+    if @selection.nil?
+      find_piece_image(@board[x, y]).displace(0, -cell_size / 4)
+      @selection = {:x => x, :y => y}
+    else
+      piece = @board[@selection[:x], @selection[:y]]
+      @selection = nil
+      this_piece_image = find_piece_image(piece)
+      this_piece_image.displace(0, 0)
+      piece.move(x, y)
+      this_piece_image.move((cell_gap * x).to_i, (cell_gap * y).to_i)
     end
   end
 
   private
-
+  
   def piece_image piece
     image "./#{piece.type}.png",
           :top => (piece.y * cell_gap).to_i,
           :left => (piece.x * cell_gap).to_i,
           :width => cell_size, :height => cell_size
+  end
+
+  def find_piece_image piece
+    x = (cell_gap * piece.x).to_i
+    y = (cell_gap * piece.y).to_i
+    @pieces.detect do |piece_image|
+      piece_image.left == x and piece_image.top == y 
+    end
   end
 
   def board_size
@@ -78,86 +109,3 @@ Shoes.app :width => 600, :height => 600 do
   @my_stack.height = height
   @my_stack.redraw
 end
-
-=begin
-Shoes.app :width => 800, :height => 600 do
-  @board = Game::Thud::Board.new
-
-  @selected = nil # no selected piece at first
-  @selected_piece = nil
-  animate(10) do |i| # animates selected piece
-    unless @selected.nil?
-      @selected.displace(0, 1 - (Math.sin(i) * 3).to_i.abs)
-    end
-  end
-
-  stack :width => 600 do # here goes the board
-    @board_size = [width, height].sort.first
-    @cell_gap = @board_size.to_f / 15
-    @cell_size = @cell_gap.to_i
-
-    board_image = image "./board.png", :top => 0, :left => 0,
-                        :width => @board_size, :height => @board_size
-
-    # clickable cells (to drop pieces)
-    fill rgb(0, 0, 0, 0)
-    15.times do |y|
-      15.times do |x|
-        if @board.cell? x, y
-          zone = rect((@cell_gap * x).to_i, (@cell_gap * y).to_i,
-               @cell_size, @cell_size)
-          zone.click do
-            debug "clickedi at #{x}, #{y}"
-            unless @selected.nil?
-              begin
-                @selected_piece.move x, y
-                @selected.move((@cell_gap * x).to_i, (@cell_gap * y).to_i)
-                @selected.displace 0, 0
-                @selected = @selected_piece = nil
-              rescue
-                warn "move incomplete"
-              end
-            end
-          end
-        end
-      end
-    end
-
-    @board.each do |piece|
-      unless piece.dead?
-        piece_image = image "./#{piece.type}.png",
-                            :top => (piece.y * @cell_gap).to_i,
-                            :left => (piece.x * @cell_gap).to_i,
-                            :width => @cell_size, :height => @cell_size
-
-        piece_image.click do # selection of a piece
-          debug "clicked the #{piece.type} at #{piece.x}, #{piece.y}"
-          if @selected.equal? piece_image
-            @selected = nil
-            @selected_piece = nil
-            piece_image.displace(0, 0)
-          else
-            @selected.displace(0, 0) unless @selected.nil?
-            @selected = piece_image
-            @selected_piece = piece
-          end
-        end
-      end
-    end
-  end
-
-  stack :width => -600 do
-    flow do # there goes outer controls
-      button("Pass")
-    end
-    flow do # a place for the dead
-      @board.each do |piece|
-        if piece.dead?
-          image "./#{piece.type}.png",
-                :width => @cell_size, :height => @cell_size
-        end
-      end
-    end
-  end
-end
-=end
